712e42d42a6f808175620a03cc7c1b0ba0bd9ef6
rafeh01 <rafehqazi1@gmail.com>
2015-12-18 04:58:04 -0600

    added more programs for cpp.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "basic_data_types/basic_data_types.h"
+#include "dictionary_map/map.h"
+#include "conditionals/conditional.h"
 
 using namespace std;
 
-int main() {
+
+void basics();
+int main () {
+    // input_output();
+    basics();
+    dict_map();
+    conditional_();
+    return 0;  // you always have to put this in c++.
+}
+
+void basics() {
     cout << "Hello, bro World!" << endl;
     const double PI = 3.1415926535;  // like float but usually accurate up to 15 digits. (8 bytes)
     char myGrade = 'A';  // 1 byte in memory
@@ -16,7 +29,9 @@ int main() {
     int five = 5;
     int num = 10;
     num += 1;
-    string hoe = "hoe";
+    string thing = "thing";
+    printf("HI person!\n");
+
 
     // =================== PRINT STUFF ========================
     cout << "Favorite number " << favNum << endl;
@@ -35,11 +50,29 @@ int main() {
     cout << "-- 5 = " << --five << endl;
     cout << "num += 1: " << num << endl;
     cout << "(1 + 3 * 5): " << 1 + 3 * 5 << endl;
-    cout << "string: " << hoe << endl;
-    return 0;  // you always have to put this in c++.
+    cout << "string: " << thing << endl;
+    cout << "4 / 5 = " << 4 / 5 << endl;
+    cout << "4 / 5 = " << (float) 4 / 5 << endl;  // how I wrote (float)... It is called casting.
+    cout << "4 / 5 = " << (double) 4 / 5 << endl;  // how I wrote (double)... It is called casting.
+    cout << "4 / 5 = " << (int) 4 / 5 << endl;  // how I wrote (int)... It is called casting.
+
+    // ternary operator
+    // variable = (condition) ? true : false
+    int bigNum = (5 > 2) ? 5 : 2;
+    cout << "bigNum: " << bigNum << endl;
+    int bigNum2 = (1 > 2) ? 5 : 2;
+    cout << "bigNum: " << bigNum2 << endl;
+
+    // Arrays
+    int array1[5];  // store 5 elements.
+    int badNums[5] = {5, 12, 10, 5, 2};
+    cout << "bad numbers: " << badNums << endl;
+    cout << "bad numbers: " << badNums[1] << endl;
+    cout << "bad numbers: " << badNums[-1] << endl;
+    cout << "bad numbers: " << badNums[4] << endl;
+    cout << "bad numbers: " << badNums[5] << endl;
+    cout << "bad numbers: " << badNums[100] << endl;
 }
-
-
 // Other types include:
 // short int: At least 16 bits
 // long int: At least 32 bits
